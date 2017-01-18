406b27c0d6d1eb5edd49d548d89570baf7513f94
Pradeep Kumar <pradeepdevelops@gmail.com>
2017-01-18 17:45:45 -0600

    end of lesson 20

--- app/todo.js
+++ app/todo.js
@@ -49,8 +49,25 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
 
 const todoApp = combineReducers({ todos, visibilityFilter })
 
-const ToDo = () => (
-	<div>Todo</div>
+
+
+const Todo = ({onClick, complete, text}) => (
+	<li onClick={onClick}
+			style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
+		{text}
+	</li>
+)
+
+const TodoList = ({todos, onTodoClick}) => (
+	<ul>
+		{todos.map(todo => 
+			<Todo
+				key={todo.id}
+				{...todo}
+				onClick={() => onTodoClick(todo.id)}
+				/>
+			)}
+	</ul>
 )
 
 const getVisibleTodos = (todos, filter) => {
@@ -103,22 +120,15 @@ class ToDoApp extends React.Component {
 				}}>
 					Add Todo
 				</button>
-				<ul>
-					{visibleTodos.map(todo => 
-						<li key={todo.id}
-								onClick={() => {
-									store.dispatch({
-										type: 'TOGGLE_TODO',
-										id: todo.id
-									})
-								}}
-								style={{
-									textDecoration: todo.completed ? 'line-through' : 'none'
-								}}>
-							{todo.text}
-						</li>
-						)}
-				</ul>
+				<TodoList 
+					todos={visibleTodos}
+					onTodoClick={id =>
+						store.dispatch({
+							type: 'TOGGLE_TODO',
+							id
+						})
+					}
+				/>
 				<p>
 					Show:
 					{' '}
